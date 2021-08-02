30fb8da873383e087cded657cdc2aaac61cc1ee4
Shubham-j88 <79858876+Shubham-j88@users.noreply.github.com>
2021-08-02 23:18:33 +0530

    Add files via upload

---
+++ ChatroomappApplication.java
@@ -0,0 +1,15 @@
+package chatroomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatroomappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatroomappApplication.class, args);
+		
+		System.out.println("hey...this is shubham");
+	}
+
+}
